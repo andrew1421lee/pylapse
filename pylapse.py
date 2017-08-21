5463cea58d2cc8236453eac16be40842e4b182f7
Anchu Lee <andrew1421lee@gmail.com>
2017-08-21 19:35:37 -0400

    comments

--- pylapse.py
+++ pylapse.py
@@ -2,30 +2,44 @@ import pygame
 import pygame.camera
 import time
 
-RES = (1280, 720)
-WINDOWSIZE = (854, 480)
-FILEPREFIX = 'capture'
+RES = (1280, 720) # image resolution
+WINDOWSIZE = (854, 480) # preview resolution
+FILEPREFIX = 'capture' # file prefix
 
 def stream():
+    # init
     pygame.init()
-    pygame.camera.init()
+    pygame.camera.init() 
+    # config window to preview size
     display = pygame.display.set_mode(WINDOWSIZE, 0)
+    # config camera to image resolution
     camera = pygame.camera.Camera(pygame.camera.list_cameras()[0], RES)
     camera.start()
+    # config screen
     screen = pygame.surface.Surface(WINDOWSIZE, 0, display)
+    # start time for recording
     starttime = int(time.time())
+    # file counter
     counter = 0
 
     while True:
+        # current time in recording for filename
         currenttime = time.localtime()
+        # current time in unix time
         newtime = int(time.time())
+        # get an image from webcame
         capture = camera.get_image()
+        # scale the image to screen
         screen = pygame.transform.scale(capture, WINDOWSIZE)
         display.blit(screen, (0,0))
         pygame.display.flip()
+        # if set duration has passed
         if newtime - starttime > 60:
+            # save the image with timestamp
             pygame.image.save(capture, FILEPREFIX + '_' + str(counter) + '_' + str(currenttime.tm_mon) + '_' + str(currenttime.tm_mday) + '_' + str(currenttime.tm_hour) + '_' + str(currenttime.tm_min) + '_' + str(currenttime.tm_sec))
+            # update time
             starttime = newtime
+            # increase file count
             counter = counter + 1
 
 
